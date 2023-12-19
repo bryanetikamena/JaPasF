0da7d3cb35898aa3e928a98c086388b2b0b69873
bryanetikamena <130908971+bryanetikamena@users.noreply.github.com>
2023-12-20 00:27:27 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,140 @@
+export const OlahragaPopuler = [
+    {
+      id: 1,
+      title: 'Basket',
+      category: 'Basket',
+      image:
+        'https://i.pinimg.com/originals/c3/bc/59/c3bc59e66d310ee9238e6af7d74da98e.jpg',
+        content: `In the ever-evolving landscape of transportation, the emergence of electric cars has sparked a revolution. Exploring the world of electric cars unveils a myriad of innovations and possibilities that are reshaping the way we perceive mobility. Electric cars, also known as electric vehicles (EVs), are automobiles powered by one or more electric motors using energy stored in rechargeable batteries. This departure from traditional internal combustion engines marks a significant shift towards sustainability and environmental consciousness. As concerns about fossil fuel depletion and climate change intensify, electric cars have emerged as a promising solution to mitigate the impact of transportation on the planet.`,
+    },
+    {
+      id: 2,
+      title: 'BUlu Tangkis',
+      category: 'BuluTangkis',
+      image:
+        'https://i.pinimg.com/564x/cd/98/af/cd98af386c80be3f7d8a8a3c2b4c6bd1.jpg',
+        content: `In the ever-evolving landscape of transportation, the emergence of electric cars has sparked a revolution. Exploring the world of electric cars unveils a myriad of innovations and possibilities that are reshaping the way we perceive mobility. Electric cars, also known as electric vehicles (EVs), are automobiles powered by one or more electric motors using energy stored in rechargeable batteries. This departure from traditional internal combustion engines marks a significant shift towards sustainability and environmental consciousness. As concerns about fossil fuel depletion and climate change intensify, electric cars have emerged as a promising solution to mitigate the impact of transportation on the planet.`,
+    },
+    {
+      id: 3,
+      title: 'Voli',
+      category: 'Voli',
+      image:
+        'https://i.pinimg.com/736x/43/22/48/4322488bdb22798914f256db7fd638bf.jpg',
+        content: `In the ever-evolving landscape of transportation, the emergence of electric cars has sparked a revolution. Exploring the world of electric cars unveils a myriad of innovations and possibilities that are reshaping the way we perceive mobility. Electric cars, also known as electric vehicles (EVs), are automobiles powered by one or more electric motors using energy stored in rechargeable batteries. This departure from traditional internal combustion engines marks a significant shift towards sustainability and environmental consciousness. As concerns about fossil fuel depletion and climate change intensify, electric cars have emerged as a promising solution to mitigate the impact of transportation on the planet.`,
+    },
+  ];
+
+export const ProfileData = {
+ image : require('./src/assets/images/fotoprofile.jpg'),
+ profilePict:
+ 'https://i.pinimg.com/originals/20/a0/30/20a030bec7fb3901eea5a3a06951e3a0.jpg',
+  name: 'Madelbertha Fridolin Puka',
+  createdAt: '14 Juli, 2003',
+  blogPosted: 29,
+  following: 3000,
+  follower: 3000,
+  };
+
+export const BeritaOlahraga = [
+    {
+      id : 1,
+      nama : "Basket",
+      no : "Olahraga Tim",
+      desc : "Basket adalah sebuah olahraga tim yang dimainkan oleh dua tim yang berusaha mencetak poin dengan melempar bola ke dalam keranjang lawan. Setiap tim terdiri dari lima pemain yang bergerak di atas lapangan berbentuk persegi panjang dan berusaha mencetak poin dengan melempar bola ke keranjang yang tinggi. ",
+      image : "https://i.pinimg.com/originals/c3/bc/59/c3bc59e66d310ee9238e6af7d74da98e.jpg"
+    },
+    {
+      id : 2,
+      nama : "Bulu Tangkis",
+      no : "Olahraga Tim ",
+      desc : "Bulu tangkis adalah olahraga raket yang dimainkan oleh dua pemain (ganda) atau empat pemain (ganda campuran) di lapangan yang dibatasi oleh jaring di tengahnya. Olahraga ini bertujuan untuk mencetak poin dengan menghantarkan kok ke lapangan lawan dan menghindari agar bola tersebut tidak jatuh di sisi lapangan mereka.",
+      image : "https://i.pinimg.com/564x/cd/98/af/cd98af386c80be3f7d8a8a3c2b4c6bd1.jpg"
+    },
+    {
+      id : 3,
+      nama : "Renang",
+      no : "Olahraga Individu dan Tim",
+      desc : "Renang adalah olahraga yang serba guna, menyenangkan, dan memberikan manfaat kesehatan. Ini adalah cara yang baik untuk meningkatkan kebugaran kardiovaskular, memperkuat otot, dan menikmati air.",
+      image : "https://i.pinimg.com/originals/49/51/17/4951179acfd6fa46ccdf8abc6e49400e.jpg"
+    },
+    {
+      id : 4,
+      nama : "Voli",
+      no : "Olahraga Tim",
+      desc : "Voli adalah olahraga yang membutuhkan keterampilan teknis, kecepatan, dan kerja sama tim. Ini merupakan olahraga yang menyenangkan, baik untuk hiburan maupun kompetisi, serta mempromosikan kebugaran fisik dan kolaborasi tim.",
+      image : "https://i.pinimg.com/736x/43/22/48/4322488bdb22798914f256db7fd638bf.jpg"
+    },
+    {
+      id : 5,
+      nama : "Sepak Bola",
+      no : "Olahraga Tim",
+      desc : "Sepak bola adalah olahraga yang mempromosikan kebugaran fisik, keterampilan teknis, dan kerja sama tim. Ini adalah olahraga yang mendunia dan menyatukan orang dari berbagai budaya dan latar belakang di seluruh dunia.",
+      image : "https://i.pinimg.com/originals/fc/a9/e9/fca9e929a9b8e67df55f1d8990de2983.jpg"
+    },
+    {
+      id : 6,
+      nama : "Tenis",
+      no : "Olahraga Tim ",
+      desc : "erpartisipasi dalam penelitian medis, mengikuti perkembangan ilmu kedokteran, dan menerapkan penemuan terbaru dalam praktik klinis.",
+      image : "https://m.caping.co.id/news/detail/12304233"
+    },
+  ];
+
+  export const BlogList = [
+    {
+      id: 1,
+      title: 'Siapkah Indonesia untuk melawan Tim Thailand?',
+      category: 'Bola Kaki',
+      image:
+        'https://i.pinimg.com/originals/fc/a9/e9/fca9e929a9b8e67df55f1d8990de2983.jpg',
+      createdAt: 'Indonesia vs Thailand',
+      totalComments: 22,
+      totalLikes:2300,
+      content: `Tim nasional Indonesia akan segera berhadapan dengan Thailand dalam sebuah pertandingan sepak bola internasional yang bergengsi. Dalam menghadapi pertandingan ini, Indonesia dihadapkan pada tantangan berat menghadapi kekuatan dan reputasi Thailand sebagai tim sepak bola terbaik di Asia Tenggara. Judul "Menghadapi Tantangan Kuat Thailand" menggambarkan tingkat kesulitan yang akan dihadapi oleh timnas Indonesia. "Siap atau Tidak, Lawan Tetap Datang" mencerminkan kenyataan bahwa meski belum tentu siap sepenuhnya, Indonesia harus tetap maju membela tanah air. Sementara judul "Bangkitkan Semangat Juang! Hadapi Harimau Asia" dimaksudkan untuk membakar semangat dan memberi motivasi pemain dan pendukung Indonesia dalam menghadapi pertandingan penting ini. Dengan persiapan matang dan tekad baja, Indonesia berpeluang meraih hasil maksimal melawan Thailand, tim kuat kawakan Asia Tenggara.Persiapan sudah dilakukan secara maksimal oleh timnas Indonesia menjelang pertandingan melawan Thailand. Ini merupakan kesempatan bagi anak asuh Garuda untuk membuktikan kesiapan dan kemampuan mereka bersaing di level tertinggi, sebagaimana tercermin dalam judul "Bersiaplah Anak Asuh Garuda! Thailand Sudah Menanti". Selaku juara bertahan dan raksasa sepak bola Asia Tenggara, tentu Thailand sangat dinantikan kehadirannya.`,
+    },
+    {
+      id: 2,
+      title: 'Atlet Favorit',
+      category: 'Bulu Tangkis',
+      image:
+        'https://i.pinimg.com/originals/f1/2c/27/f12c27978ffb271f9ce92558db24780e.jpg',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 88,
+      totalLikes:800,
+      content: ` Anthony Ginting.`,
+    },
+    {
+      id: 3,
+      title: 'Turnamen Besar',
+      category: 'Bulu Tangkis',
+      image:
+        'https://i.pinimg.com/originals/de/51/ee/de51eeb21e1d3ff0433522131a916ae1.jpg',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+      totalLikes:2300,
+      content: ` Piala Dunioa Bulu Tangkis 2023`,
+    },
+    {
+      id: 4,
+      title: 'Berita Olahraga Terbaru',
+      category: 'Atlet',
+      image:
+        'https://i.pinimg.com/originals/08/6b/49/086b49e47ebab8c0c7a5657515f541fe.jpg',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+      totalLikes:2300,
+      content: `Atlet angkat besi asal Gorontalo raih emas SEA GAmes 2023 `,
+    },
+    {
+      id: 5,
+      title: 'Jadwal Pertandingan Minggu Ini',
+      category: 'catur',
+      image:
+        'https://i.pinimg.com/originals/38/a7/da/38a7da28b9d8ff12d2bce8ff2ea35c6e.jpg',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+      totalLikes:2300,
+      content: `Turnamen Catur Tingkat Nasional`,
+    },
+  ];
